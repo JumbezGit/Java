90df30fc4159f67121de2be7a0ddcfdfbf5aa26e
jumbez <jumbezson@gmail.com>
2025-12-15 13:06:46 +0300

    String

---
+++ String/MyString.java
@@ -0,0 +1,8 @@
+public class MyString{
+    public static void main(String[] args){
+        String str = "Hello Jumbez";
+        String UpperCase = str.toUpperCase();
+        System.out.println(UpperCase);
+
+    }
+}
